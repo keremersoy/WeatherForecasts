32db2bae801bfb535859c7b647aabd6406df35ef
kereme <kereme@DESKTOP-OD7R3FD>
2022-07-29 08:58:26 +0300

    Add project files.

---
+++ WeatherForecasts.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeatherForecasts.Domain.Abstractions;
+using WeatherForecasts.Domain.Models;
+
+namespace WeatherForecasts.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IWeatherForecastService _service;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger,IWeatherForecastService service)
+        {
+            _service = service;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return _service.ProcessFtemperature();
+        }
+    }
+}
---
+++ WeatherForecasts.Service/WeatherForecastService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherForecasts.Domain.Abstractions;
+using WeatherForecasts.Domain.Models;
+
+namespace WeatherForecasts.Service
+{
+    public class WeatherForecastService : IWeatherForecastService
+    {
+        private readonly IWeatherForecastRepository _repo;
+        public WeatherForecastService(IWeatherForecastRepository repo)
+        {
+            _repo = repo;
+        }
+        public List<WeatherForecast> ProcessFtemperature()
+        {
+            var forecasts = _repo.GetForecasts();
+            var processed = new List<WeatherForecast>();
+            foreach (var f in forecasts)
+            {
+                f.TemperatureF = 32 + (int)(f.TemperatureC / 0.5556);
+                processed.Add(f);
+            }
+            return processed;
+        }
+    }
+}
